8cec70b84e9bf527dc44cb3a9596e65fc7cf80c3
ggedde <ggedde@users.noreply.github.com>
2024-11-27 19:09:21 -0800

    Updates to Collapses and Toggles.

--- docs/docs.js
+++ docs/docs.js
@@ -55,7 +55,7 @@ function cleanContent(html) {
     html = html.replaceAll('bg-faint round p-1 ', '');
     html = html.replaceAll(' bg-faint round p-1', '');
     html = html.replaceAll('<div class="code-resize-handle"></div>', '');
-    var firsTag = html.indexOf('&');
+    var firsTag = html.indexOf('&lt');
     if (firsTag === -1) {
         firsTag = html.indexOf('<');
     }
@@ -103,8 +103,6 @@ function cleanContent(html) {
     // html = html.replaceAll('<!-- nl -->', "\n");
     // html = html.replaceAll('<!---->', "\t");
     html = html.replaceAll(' data-enpassusermodified="yes"', '');
-    html = html.replaceAll(/&ltdiv class="note p-2 mb-n2 mx-n2 mt-2 bg-faint"&gt.*&lt\/div&gt/gs, '');
-    html = html.replaceAll(/\<div class="note p-2 mb-n2 mx-n2 mt-2 bg-faint"\>.*\<\/div\>/gs, '');
 
     html = html.replaceAll('&ltdiv class="code-resize-handle"&gt&lt/div&gt', '###');
     html = html.replace(/\&gt\n.*\#\#\#/, '&gt');
@@ -162,6 +160,7 @@ function loadCodeContainer(el) {
     var codeDiv = elem.querySelector('.code-preview-container');
     var html = getContent(el);
     html = html.replaceAll('<', '&lt').replaceAll('>', '&gt');
+
     var codeDivContents = '<pre class="mb-0"><code class="language-html'+(elem.classList.contains('with-wrap')?' pre-wrap':'')+'">' + html + '</code></pre>';
 
     codeDiv.innerHTML = codeDivContents;
@@ -172,7 +171,9 @@ document.querySelectorAll('.show-code').forEach((elem, index) => {
     var toggleId = Math.random().toString().replace('.', '');
     var badge = (elem.hasAttribute('data-badge') ? '<'+(elem.hasAttribute('data-badge-link') ? 'a title="Link to '+elem.getAttribute('data-badge-link')+'" aria-label="'+elem.getAttribute('data-badge')+'" href="'+elem.getAttribute('data-badge-link')+'"' : 'span')+' class="badge dense grey outline xs ml-1 align-text-bottom">'+elem.getAttribute('data-badge')+(elem.hasAttribute('data-badge-link') ? '</a>' : '</span>') : '');
     var tooltipWarning = (elem.hasAttribute('data-tooltip-warning') ? '<span class="sm"><i class="icon color-secondary shy"><svg viewBox="0 0 24 24"><path d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10 10 10 0 0 0 10-10A10 10 0 0 0 12 2m-1 15h2v-6h-2v6z" /></svg></i><span class="tooltip sm center-x outset-top">'+elem.getAttribute('data-tooltip-warning')+'</span></span>' : '');
-    var titleNote = (elem.hasAttribute('data-title-note') ? '<div class="note xs my-1 color-grey">'+elem.getAttribute('data-title-note')+'</div>' : '');
+    var note = (elem.hasAttribute('data-note') ? '<p>'+elem.getAttribute('data-note')+'</p>' : '');
+    var warning = (elem.hasAttribute('data-warning') ? '<p class="color-warning">'+elem.getAttribute('data-warning')+'</p>' : '');
+    var footerNote = (elem.hasAttribute('data-footer-note') ? '<p>'+elem.getAttribute('data-footer-note')+'</p>' : '');
     var codeDiv = '<div class="content"><div class="code-preview-container"></div></div>';
 
     var innerContents = elem.innerHTML;
@@ -181,6 +182,11 @@ document.querySelectorAll('.show-code').forEach((elem, index) => {
     var languages = elem.querySelectorAll('.language-select');
 
     var languageSelector = '';
+    var headerNote = warning || note ? '<div class="note bg-faint bb-1 py-2 mt-0">' + warning + note + '</div>' : '';
+    var footerNote = footerNote ? '<div class="note bg-faint bt-1 py-2 mt-0">' + footerNote + '</div>' : '';
+
+    var headerNotes = headerNote;
+    var footerNotes = footerNote;
 
     var languageNames = {
         html: 'HTML',
@@ -191,14 +197,29 @@ document.querySelectorAll('.show-code').forEach((elem, index) => {
         alpineJs: 'Alpine.js',
     };
 
-    if (languages && languages.length) {
+    var languageKey = '';
+    var languageNote = '';
+    var languageWarning = '';
+    var languageFooterNote = '';
+
+    if (languages && languages.length) {    
         languageSelector += '<div class="items-center flex mr-1"><select class="language-selector dense pr-3 border/20" onchange="loadCodeContainer(this)">';
         languages.forEach(language => {
-            languageSelector += '<option value="'+language.getAttribute('data-language')+'">'+languageNames[language.getAttribute('data-language')]+'</option>';
+            languageKey = language.getAttribute('data-language');
+            languageSelector += '<option value="'+languageKey+'">'+languageNames[languageKey]+'</option>';
+            languageNote = (language.hasAttribute('data-note') ? '<p>'+language.getAttribute('data-note')+'</p>' : '');
+            languageWarning = (language.hasAttribute('data-warning') ? '<p class="color-warning">'+language.getAttribute('data-warning')+'</p>' : '');
+            languageFooterNote = (language.hasAttribute('data-footer-note') ? '<div class="language-'+languageKey+' note p-2 bg-faint mt-0">'+language.getAttribute('data-footer-note')+'</div>' : '');
+
+            headerNotes += languageNote || languageWarning ? '<div class="language-'+languageKey+' note bg-faint bb-1 py-2 mt-0">' + languageNote + languageWarning + '</div>' : '';
+            footerNotes = languageFooterNote + footerNotes;
         });
         languageSelector += '</select></div>';
     }
-    elem.outerHTML = '<article class="mb-3 outline g-0 code-content-container" id="code-container-'+index+'"><header class="pr-1 sm md:md"><h4>'+elem.getAttribute('data-title')+' '+badge+tooltipWarning+titleNote+'</h4><div class="no-wrap flex">'+languageSelector+'<button class="shy icon link" title="Toggle Theme" onclick="toggleTheme(this.parentElement.parentElement.parentElement);"><svg viewBox="0 0 24 24"><path d="M12,18V6A6,6 0 0,1 18,12A6,6 0 0,1 12,18M20,15.31L23.31,12L20,8.69V4H15.31L12,0.69L8.69,4H4V8.69L0.69,12L4,15.31V20H8.69L12,23.31L15.31,20H20V15.31Z" /></svg></button><button onclick="loadCodeContainer(this)" data-toggle="#code-'+toggleId+'" class="shy icon link" title="Show HTML code"><svg class="lg" viewBox="0 0 24 24"><path d="m14.6 16.6 4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4m-5.2 0L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4z" /></svg></button><button class="shy icon link" title="Copy HTML to Clipboard" onclick="copyCode(event); setTimeout(() => {this.classList.remove(\'open\'); this.classList.remove(\'active\'); this.setAttribute(\'aria-pressed\', false)}, 2000)" data-toggle><svg viewBox="0 0 24 24"><path d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1z" /></svg><svg viewBox="0 0 24 24"><path d="M21 7 9 19l-5.5-5.5 1.41-1.41L9 16.17 19.59 5.59 21 7z" /></svg></button></div></header><div class="p-0"><div id="code-'+toggleId+'" class="code-container collapse">'+codeDiv+'</div></div><div class="p-2 code-content">'+innerContents+'<div class="code-resize-handle"></div></div>'+(elem.hasAttribute('data-note') ? '<div class="note p-2 bg-faint">'+elem.getAttribute('data-note')+'</div>' : '')+'</article>';
+
+    var innerContents = elem.innerHTML;
+
+    elem.outerHTML = '<article class="mb-3 outline g-0 code-content-container" id="code-container-'+index+'"><header class="pr-1 sm md:md"><h4>'+elem.getAttribute('data-title')+' '+badge+tooltipWarning+'</h4><div class="no-wrap flex">'+languageSelector+'<button class="shy icon link" title="Toggle Theme" onclick="toggleTheme(this.parentElement.parentElement.parentElement);"><svg viewBox="0 0 24 24"><path d="M12,18V6A6,6 0 0,1 18,12A6,6 0 0,1 12,18M20,15.31L23.31,12L20,8.69V4H15.31L12,0.69L8.69,4H4V8.69L0.69,12L4,15.31V20H8.69L12,23.31L15.31,20H20V15.31Z" /></svg></button><button onclick="loadCodeContainer(this)" data-toggle="#code-'+toggleId+'" class="shy icon link" title="Show HTML code"><svg class="lg" viewBox="0 0 24 24"><path d="m14.6 16.6 4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4m-5.2 0L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4z" /></svg></button><button class="shy icon link" title="Copy HTML to Clipboard" onclick="copyCode(event); setTimeout(() => {this.classList.remove(\'open\'); this.classList.remove(\'active\'); this.setAttribute(\'aria-pressed\', false)}, 2000)" data-toggle><svg viewBox="0 0 24 24"><path d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1z" /></svg><svg viewBox="0 0 24 24"><path d="M21 7 9 19l-5.5-5.5 1.41-1.41L9 16.17 19.59 5.59 21 7z" /></svg></button></div></header>'+headerNotes+'<div class="p-0 mt-0"><div id="code-'+toggleId+'" class="code-container collapse">'+codeDiv+'</div></div><div class="p-2 code-content">'+innerContents+'<div class="code-resize-handle"></div></div>'+footerNotes+'</article>';
 });
 
 document.querySelectorAll('.code-resize-handle').forEach((elem) => {
